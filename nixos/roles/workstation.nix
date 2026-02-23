9826c952fd6f52d669a97cb4aae06270b65db114
lostduk <hi@lostduk.com>
2026-02-23 13:11:37 +0000

    feat(nixos/roles/workstation): wipe trash service

--- nixos/roles/workstation.nix
+++ nixos/roles/workstation.nix
@@ -1,4 +1,4 @@
-{ inputs, outputs, config, lib, ... }:
+{ inputs, outputs, config, pkgs, lib, ... }:
 
 let
   roleName = "workstation";
@@ -13,6 +13,22 @@ in {
       extraSpecialArgs = { inherit inputs outputs; };
 
       users.lostduk = outputs.homeConfigurations.${config.networking.hostName};
+    };
+
+    systemd.services.wipe-trash = {
+      restartIfChanged = false;
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = pkgs.writeShellScript "" ''
+          find '/home/lostduk' -mindepth 1 -maxdepth 1 \
+            ! -name 'documents' \
+            ! -name '.gnupg' \
+            ! -name '.password-store' \
+            -exec rm -r {} +
+        '';
+      };
     };
   })];
 }
